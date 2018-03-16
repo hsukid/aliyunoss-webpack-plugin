4d02a34806bcfbfa0be0b254a6c723a69ab5123a
hsukid <hyperkidom@gmail.com>
2018-03-16 10:01:27 +0800

    Update file.js
    改进文件获取，可以递归遍历目录并记录所有获取到的文件路径

--- utils/file.js
+++ utils/file.js
@@ -4,8 +4,40 @@ var path = require('path');
 var glob = require("glob");
 
 exports.eachFileSync = function(dir, callback) {
-	glob.sync(dir,{nodir:true}).map(function(el){
-		callback(el);
-	});
+	var files = fileDisplay(dir)
+	files.forEach(function(file) {
+		callback(file)
+	})
 }
 
+/** 
+ * 文件遍历方法 
+ * @param filePath 需要遍历的文件路径 
+ */  
+function fileDisplay(dir){
+	var checks = []
+	function getFiles(dir) {
+		var files = fs.readdirSync(dir);
+		//遍历读取到的文件列表  
+		files.forEach(function(file){
+			var filePath = dir
+			//获取当前文件的绝对路径  
+			//var filedir = path.join(filePath, file);  
+			var filedir = `${filePath}/${file}`;  
+			//根据文件路径获取文件信息，返回一个fs.Stats对象  
+			var stats = fs.statSync(filedir);
+			var isFile = stats.isFile();//是文件  
+			var isDir = stats.isDirectory();//是文件夹  
+			if(isFile){
+				checks.push(filedir)
+			}  
+			if(isDir){
+					filePath = filedir 
+					getFiles(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件  
+			}  
+		})
+	}
+	getFiles(dir)
+
+	return checks;
+}  
